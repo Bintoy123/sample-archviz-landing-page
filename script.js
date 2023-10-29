a52509fe25f69200151f1521ac50d4be3adee0f3
Vincient Degala <98757981+Bintoy123@users.noreply.github.com>
2023-10-29 19:38:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// burger function
+function myFunction(){
+  var x = document.getElementById("myLink");
+  if (x.style.display === "block"){
+    x.style.display = "none";
+  }else{
+    x.style.display = "block";
+  }
+}
+
 var panels = document.querySelectorAll(".three");
 
 panels.forEach((panel) =>{
